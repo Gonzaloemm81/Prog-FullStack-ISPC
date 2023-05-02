ac38e42e04988dd4f0981d1605b1a1c8a5d1e9b7
Brian <polobrallardbrian@gmail.com>
2023-05-02 17:02:26 -0300

    agregue inicio sesion

--- FLE-angular/src/app/pages/pages.module.ts
+++ FLE-angular/src/app/pages/pages.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegistroComponent } from './registro/registro.component';
 import { CarruselComponent } from './carrusel/carrusel.component';
+import { IniciosesionComponent } from './iniciosesion/iniciosesion.component';
 
 
 
 @NgModule({
   declarations: [
     RegistroComponent,
-    CarruselComponent
+    CarruselComponent,
+    IniciosesionComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     RegistroComponent,
-    CarruselComponent
+    CarruselComponent,
+    IniciosesionComponent
   ]
 })
 export class PagesModule { }
