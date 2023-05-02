8d30d46f877c9dc4d2fd859f2caec2ca7cfeced3
Gonzalo Ramírez <gonzalo.emm81@gmail.com>
2023-05-02 10:04:57 -0300

    agregué el modulo Pages y el componente registro

--- FLE-angular/src/app/app.module.ts
+++ FLE-angular/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    SharedModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
