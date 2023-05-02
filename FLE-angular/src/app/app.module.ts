dc3da8f39b9b91618da064adc6399159afddbfed
Gonzalo Ramírez <gonzalo.emm81@gmail.com>
2023-05-02 17:38:23 -0300

    cree rama GonzaloRam

--- FLE-angular/src/app/app.module.ts
+++ FLE-angular/src/app/app.module.ts
@@ -3,10 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { PagesModule } from './pages/pages.module';
-<<<<<<< HEAD
 
-=======
->>>>>>> 9eb44713763d32b194d24609fb9052f64be896e7
 @NgModule({
   declarations: [
     AppComponent
--- FLE-angular/src/app/pages/pages.module.ts
+++ FLE-angular/src/app/pages/pages.module.ts
@@ -1,34 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
 import { RegistroComponent } from './registro/registro.component';
 import { CarruselComponent } from './carrusel/carrusel.component';
-=======
-import { IniciosesionComponent } from './iniciosesion/iniciosesion.component';
->>>>>>> 9eb44713763d32b194d24609fb9052f64be896e7
 import { IniciosesionComponent } from './iniciosesion/iniciosesion.component';
 
 
 
 @NgModule({
   declarations: [
-<<<<<<< HEAD
     RegistroComponent,
     CarruselComponent,
     IniciosesionComponent
->>>>>>> 9eb44713763d32b194d24609fb9052f64be896e7
   ],
   imports: [
     CommonModule
   ],
   exports: [
-<<<<<<< HEAD
     RegistroComponent,
     CarruselComponent,
     IniciosesionComponent
-=======
-    IniciosesionComponent
->>>>>>> 9eb44713763d32b194d24609fb9052f64be896e7
   ]
 })
 export class PagesModule { }
