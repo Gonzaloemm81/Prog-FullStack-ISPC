67d3ad262ace80809bbf8f6c8560369865a98064
Gonzalo Ramírez <gonzalo.emm81@gmail.com>
2023-05-05 17:21:38 -0300

    arreglo de css y routing

--- FLE-angular/src/app/app-routing.module.ts
+++ FLE-angular/src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { IniciosesionComponent } from './pages/iniciosesion/iniciosesion.component';
 import { ServiciosComponent } from './pages/servicios/servicios.component';
+import { SeguimientoComponent } from './pages/seguimiento/seguimiento.component';
+import { CalculadoraComponent } from './pages/calculadora/calculadora.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 const routes: Routes = [{
     component: RegistroComponent,
@@ -15,7 +18,20 @@ const routes: Routes = [{
   {
     component: ServiciosComponent,
     path: 'servicios'
+  },
+  {
+    component: SeguimientoComponent,
+    path: 'seguimiento'
+  },
+  {
+    component: CalculadoraComponent,
+    path: 'calculadora'
+  },
+  {
+    component: FooterComponent,
+    path: 'footer'
   }
+
   ];
 
   @NgModule({
