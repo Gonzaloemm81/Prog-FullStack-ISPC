8a3772ca24f100ae2d9c2f07c33b80bc118b1db4
EmiNorte <106417792+EmiNorte@users.noreply.github.com>
2023-06-17 19:53:53 -0300

    Update shared.module.ts

--- FLE-angular/src/app/shared/shared.module.ts
+++ FLE-angular/src/app/shared/shared.module.ts
@@ -3,20 +3,24 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { PagesModule } from "../pages/pages.module";
 
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    NavbarComponent,
-    FooterComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    HeaderComponent,FooterComponent,
-    NavbarComponent
-  ]
+    declarations: [
+        HeaderComponent,
+        NavbarComponent,
+        FooterComponent,
+        InicioComponent
+    ],
+    exports: [
+        HeaderComponent, FooterComponent,
+        NavbarComponent, InicioComponent
+    ],
+    imports: [
+        CommonModule,
+        PagesModule
+    ]
 })
 export class SharedModule { }
