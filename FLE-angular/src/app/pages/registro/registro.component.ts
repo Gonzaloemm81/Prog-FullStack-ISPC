079bd9a547ad759538bdaaf5fdf5d39b8a7a3aab
Gonzaloemm81 <105302887+Gonzaloemm81@users.noreply.github.com>
2023-06-17 20:25:33 -0300

    Update registro.component.ts

--- FLE-angular/src/app/pages/registro/registro.component.ts
+++ FLE-angular/src/app/pages/registro/registro.component.ts
@@ -1,49 +1,86 @@
 import { Component, OnInit } from '@angular/core';
+
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { UsuariosService } from 'src/app/services/usuarios.service';
+
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
-export class RegistroComponent implements OnInit {
-  registroForm: FormGroup;
+export class RegistroComponent implements OnInit{
+    // Formulario de nuevo usuario
+    usuarioForm: FormGroup;
+    userForm: boolean = false;
+  
+    usuarios: any; 
+  
+    constructor(private user: UsuariosService, 
+      private formBuilder: FormBuilder,
+      private router: Router){
 
-  constructor(private formBuilder: FormBuilder) { 
-    this.registroForm = this.formBuilder.group({
-    });
 
+// Validacion del formulario de nuevos usuarios
+this.usuarioForm = this.formBuilder.group({
+user: ['',[Validators.required, Validators.minLength(6), Validators.maxLength(20), Validators.pattern('^[a-zA-Z0-9]*$')]],
+email: ['',[Validators.required, Validators.email]] ,
+password: ['',[Validators.required,Validators.minLength(8)]],
+is_staff: ['',[Validators.required]],
+})
+
+
+}
+
+get User(){
+  return this.usuarioForm.get('user');
+}
+get Email(){
+  return this.usuarioForm.get('email');
+}
+get Password(){
+  return this.usuarioForm.get('password');
+}
+
+abrirForm(){
+  this.userForm = !this.userForm
+}
+
+
+
+
+
+ngOnInit(): void {}
+
+
+agregarUsuario() {
+  if (this.usuarioForm.invalid) {
+    this.usuarioForm.markAllAsTouched(); // Clear
+    return;
   }
-  ngOnInit() {
-    this.registroForm = this.formBuilder.group({
-      idUsuario: ['', Validators.required],
-      usuario: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
-      email: ['', [Validators.required, Validators.email]],
-      contrasena: ['', [Validators.required, Validators.minLength(6), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/)]],
-      confirmContrasena: ['', Validators.required]
-    }, {
-      validator: this.mustMatch('contrasena', 'confirmContrasena')
-    });
-  }
 
-  onRegistro(event: any) {
+  const nuevoUsuario = {
+    username: this.usuarioForm.value.user,
+    email: this.usuarioForm.value.email,
+    password: this.usuarioForm.value.password,
+    is_staff: this.usuarioForm.value.is_staff // Cambia esto según la selección del usuario en el formulario
+  };
 
-  }
+  this.user.agregarUsuarios(nuevoUsuario).subscribe(
+    response => {
+      // Manejar la respuesta exitosa
+      console.log(response);
+      alert("¡Usuario creado con éxito!")
+      this.usuarioForm.reset();
+      this.router.navigate(['/iniciosesion'])
+      
 
-  mustMatch(controlName: string, matchingControlName: string) {
-    return (formGroup: FormGroup) => {
-      const control = formGroup.controls[controlName];
-      const matchingControl = formGroup.controls[matchingControlName];
-
-      if (matchingControl.errors && !matchingControl.hasError('mustMatch')){
-        return;
-      }
-
-      if (control.value !== matchingControl.value) {
-        matchingControl.setErrors({ mustMatch: true });
-      } else {
-        matchingControl.setErrors(null);
-      }
-    };
-  }
+    },
+    error => {
+      // Manejar el error
+      console.error(error);
+    }
+  );
 }
