bee819c11e3b7455f246afbf5dcbc403a4854bdc
Gonzalo Ramírez <gonzalo.emm81@gmail.com>
2023-06-17 22:04:31 -0300

    actualizacion del back

--- BACKEND/Django/fle/AppFLE/models.py
+++ BACKEND/Django/fle/AppFLE/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(max_length=150, unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username','password']
 
 # Clases creadas por Emiliano Ramírez
 class Clientes(models.Model):
---
+++ BACKEND/Django/fle/AppFLE/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model 
+from django.contrib.auth.hashers import make_password
+from .models import Servicios
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    
+    email = serializers.EmailField(required=True)
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(min_length=4)
+    is_staff = serializers.BooleanField()
+
+    def validate_password(self, value):
+        return make_password(value)
+
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'email', 'username', 'is_staff', 'password')
+    
+
+class ServiciosSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Servicios
+        fields= '__all__'
