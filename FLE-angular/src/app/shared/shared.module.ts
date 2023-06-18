0dfa6967e3c89656f10d2288039a12cb75d3877f
Cristianp9927 <106997073+Cristianp9927@users.noreply.github.com>
2023-06-17 21:31:52 -0300

    Update shared.module.ts

--- FLE-angular/src/app/shared/shared.module.ts
+++ FLE-angular/src/app/shared/shared.module.ts
@@ -3,24 +3,20 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { InicioComponent } from './inicio/inicio.component';
-import { PagesModule } from "../pages/pages.module";
-
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [
         HeaderComponent,
         NavbarComponent,
-        FooterComponent,
-        InicioComponent
+        FooterComponent
     ],
     exports: [
         HeaderComponent, FooterComponent,
-        NavbarComponent, InicioComponent
+        NavbarComponent
     ],
     imports: [
-        CommonModule,
-        PagesModule
+        CommonModule, RouterModule
     ]
 })
 export class SharedModule { }
